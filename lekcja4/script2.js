47c3355109388de1769ea5ab26517247284d9093
Michał Różański <rozanski.michal.mr@gmail.com>
2018-09-22 13:53:48 +0200

    Introduce arythmetic operators

---
+++ lekcja4/script2.js
@@ -0,0 +1,30 @@
+var a;
+var b ="";
+
+console.log(a===b);
+
+a = "abc";
+b ="ABC";
+console.log(a>=b);
+
+
+a = "3";
+b ="ABC";
+console.log(a<b);
+
+a = "4";
+b ="30";
+console.log(a<b);
+
+
+a = 4;
+b =30;
+console.log(a<b);
+
+a = 1;
+b ="1";
+console.log(a == b);
+
+a = 1;
+b ="1";
+console.log(a === b);
