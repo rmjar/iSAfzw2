a18bc7cea1fbd6652060a230be8a782b4603f365
Michał Różański <rozanski.michal.mr@gmail.com>
2018-09-22 10:51:34 +0200

    variable conversions

--- lekcja4/script.js
+++ lekcja4/script.js
@@ -1,1 +1,33 @@
-alert("Hello, World!");
+//alert("Hello, World!");
+
+//var testType = null;
+//console.log('Type is:', typeof testType);
+
+
+// primitives through values
+var a = 1;
+var b = a;
+
+b++;
+console.log(a, b);
+
+var c = a + b;
+
+console.log(c);
+
+var d = "Hello, Variable!";
+var b = 2 + 5;
+
+//alert(d);
+console.log(d, b);
+
+
+var num = 1; // literal declaration
+var num2 = Number(1); // 
+console.log(typeof num,typeof num2);
+
+var ar1=[1];
+var ar2=[2];
+
+console.log(Number(ar1) + Number(ar2));
+
