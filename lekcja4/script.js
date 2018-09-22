7fa4f977d15975f6a0e30f0837afce291f639eec
Michał Różański <rozanski.michal.mr@gmail.com>
2018-09-22 13:38:40 +0200

    Function exercises

--- lekcja4/script.js
+++ lekcja4/script.js
@@ -1,17 +1,24 @@
 
-let myTypeOfEC6 = (input) => typeof input;
+let myTypeOfEC6 = input => typeof input;
 
 function myTypeOfClassic(input) {
     return typeof input;
 }
 
-var myTypeOfAssignment = function(input) {
+var myTypeOfAssignment = function (input) {
     return typeof input;
 }
 
 
+let mySum = (a, b) => {
+    if (typeof a != 'number' || typeof b !='number') {
+        return ("Wrong arguments!");
+    }
+    return a + b;
+}
 
 
 console.log(myTypeOfEC6(12));
 console.log(myTypeOfClassic("Hello"));
 console.log(myTypeOfAssignment(true));
+console.log(mySum("www", NaN));
