a6cfa3988c6a180721aacacd09c2c070d0641e97
Michał Różański <rozanski.michal.mr@gmail.com>
2018-09-22 11:11:32 +0200

    ES6

--- lekcja4/script.js
+++ lekcja4/script.js
@@ -1,33 +1,8 @@
-//alert("Hello, World!");
+// ES5:
+var foo = "foo";
 
-//var testType = null;
-//console.log('Type is:', typeof testType);
-
-
-// primitives through values
-var a = 1;
-var b = a;
-
-b++;
-console.log(a, b);
-
-var c = a + b;
-
-console.log(c);
-
-var d = "Hello, Variable!";
-var b = 2 + 5;
-
-//alert(d);
-console.log(d, b);
-
-
-var num = 1; // literal declaration
-var num2 = Number(1); // 
-console.log(typeof num,typeof num2);
-
-var ar1=[1];
-var ar2=[2];
-
-console.log(Number(ar1) + Number(ar2));
-
+// ES6 only;
+const BAR = "I'm BAR";
+let baz ="boo";
+baz = "booze";
+console.log(baz);
