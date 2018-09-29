5db53a2b60a9c996290a4db5b9830111997325a8
Michał Różański <rozanski.michal.mr@gmail.com>
2018-09-29 08:59:13 +0200

    For loops w1-2

---
+++ lekcja4/loops.js
@@ -0,0 +1,12 @@
+var numbers = [1, 2, 3, 4, 5];
+
+console.log("For loop with 5:")
+for (var i = 0; i < 5; i++) {
+    console.log(numbers[i]);
+}
+
+
+console.log("For loop with length:")
+for (var i = 0; i < numbers.length; i++) {
+    console.log(numbers[i]);
+}
