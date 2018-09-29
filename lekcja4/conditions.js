41ba623fd5591a0c34a240a2963703b008c1c423
Michał Różański <rozanski.michal.mr@gmail.com>
2018-09-29 08:59:13 +0200

    For loops w1-2

--- lekcja4/conditions.js
+++ lekcja4/conditions.js
@@ -1,5 +1,5 @@
 const truthy = true;
-let a = 1;
+let a = NaN;
 
 
 if (truthy) {
@@ -11,12 +11,30 @@ if (a) {
     console.log("???? ", 4);
 }
 
-a = "12";
+a = "1";
+console.log("If-else:")
 if (a === 1) {
     console.log("it's number 1");
 } else if (a === '1') {
     console.log("it's \"1\"");
 } else {
     console.log("???");
-    
+
 }
+
+console.log("Switch:")
+
+switch (a) {
+
+    case "1":
+        console.log("it's \"1\"");
+        break;
+    case 1:
+        console.log("it's number 1");
+        break;
+    default:
+        console.log("???");
+}
+
+console.log("Ternary:");
+console.log(a ? "Hello" : "Bye");
---
+++ lekcja5/loops.js
@@ -0,0 +1,44 @@
+
+const a = [];
+
+for (let i = 0; i < 1000; i++) {
+    a.push(Math.floor((Math.random() * 1000) + 1));
+}
+
+
+
+
+
+function workshop1() {
+
+    var numbers = new Array(1000).fill();
+    sleep(1000);
+    console.time('before loop');
+    var numLength = numbers.length;
+    //console.log(length);
+    for (var i = 0; i < numLength; i++) {
+        console.log(numbers[i]);
+    }
+    console.timeEnd('before loop');
+
+
+
+    var numbers2 = new Array(1000).fill();
+    sleep(1000);
+    console.time('in loop');
+    for (var i = 0; i < numbers2.length; i++) {
+        console.log(numbers2[i]);
+    }
+    console.timeEnd('in loop');
+}
+
+
+
+function sleep(milliseconds) {
+    var start = new Date().getTime();
+    for (var i = 0; i < 1e7; i++) {
+        if ((new Date().getTime() - start) > milliseconds) {
+            break;
+        }
+    }
+}
