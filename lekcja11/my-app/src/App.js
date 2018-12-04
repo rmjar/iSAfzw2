99994425bdeb448809a3c39c7271476c7dae5c06
Michał Różański <rozanski.michal.mr@gmail.com>
2018-12-04 18:41:53 +0100

    Fix lesson11 files

--- lekcja11/my-app/src/App.js
+++ lekcja11/my-app/src/App.js
@@ -1,16 +1,41 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
 import './App.css';
 
-// const name = 'Michał' const count = (a, b) => a + b; // const person1 = {
-// name: "Jan",   surname: "Kowalski" } const person2 = { // name: "Paweł",
-// surname: "Nowak" } function Button(props) {     const {label: { age }, name}
-// = props;     return (         <div>{age} {name}</div>  ); } function
-// Person(props) {     const {         age,         person: {         name,
-// surname         }     } = props;     return (   <div>{name} {surname}
-// {age}</div>     ); } const numbers = [1, 2, 5, 7, 123]; function
-// Number(props) {     const {value} = props;     return <div>{value}</div> }
-// function NumberList() {     return numbers.map((value, index) => { return
-// <Number value={value} key={index}/>     }) }
+const name = 'Michał';
+const count = (a, b) => a + b; 
+const person1 = {
+    name: "Jan", surname: "Kowalski"
+} 
+
+const person2 = {
+    name: "Paweł",
+    surname: "Nowak"
+} 
+
+function Button(props) {
+    const { label: { age }, name }
+        = props; return (<div>{age} {name}</div>);
+} 
+
+function
+    Person(props) {
+        const { age, person: { name,
+            surname } } = props; return (<div>{name} {surname}
+                {age}</div>);
+} 
+
+const numbers = [1, 2, 5, 7, 123]; 
+
+function Number(props) { 
+    const { value } = props; 
+    return <div>{value}</div> 
+}
+
+    function NumberList() {
+    return numbers.map((value, index) => {
+        return <Number value={value} key={index} />
+    })
+}
 
 const people = [
     {
@@ -28,27 +53,47 @@ const people = [
     }
 ]
 
-// function PersonToList(props) {     const {name, age} = props.data;     return
-// <div>{name} {age}</div> } function PersonList() {     return
-// people.map((person) => {         const {             id,             ...data
-// } = person;         return <PersonToList data={data} key={id}/>     }) }
-// const menuItems = [     {         label: "Item 1",         expandable: true,
-// id: 1     }, {         label: "Item 2",         expandable: false, id: 2 },
-// {         label: "Item 3",         expandable: true, id: 3     } ] const
-// SingleItem = (props) => <span>{props.label} {props.expandable ? ' v' : '  '}
-// |</span> const Items = () => menuItems.map(({id, ...item}) => <SingleItem
-// {...item} key={id}/>) function App() {     return (         <div> App <Person
-// person={{                 name: "Jan", surname: "Kowalski"  }} age={40}/>
-// <Person person={{     name: "Paweł",       surname: "Nowak"  }} age={30}/>
-// <PersonList/> <Items/>         </div> ); }
+function PersonToList(props) {
+    const { name, age } = props.data; 
+    return <div>{name} {age}</div>
+} 
+
+function PersonList() {
+    return
+    people.map((person) => {
+        const { id, ...data
+        } = person; return <PersonToList data={data} key={id} />
+    })
+}
+
+const menuItems = [{
+    label: "Item 1", expandable: true,
+    id: 1
+}, { 
+    label: "Item 2", 
+    expandable: false, 
+    id: 2 }, { 
+    label: "Item 3", 
+    expandable: true, 
+    id: 3 }] 
+
+const SingleItem = (props) => <span>{props.label} {props.expandable ? ' v' : '  '} </span>;
+
+const Items = () => menuItems.map(({ id, ...item }) => <SingleItem {...item} key={id} />);
+            
+            function App1() {
+                return (<div> App <Person
+                    person={{ name: "Jan", surname: "Kowalski" }} age={40} />
+                    <Person person={{ name: "Paweł", surname: "Nowak" }} age={30} />
+                    <PersonList /> <Items />         </div>);
+            }
 
 class List extends Component {
     render() {
         return <div>List siemanko {this.props.data}</div>
     }
 }
-
-class PersonToList extends Component {
+class PersonToList2 extends Component {
 
     constructor(props) {
         super(props);
@@ -70,8 +115,7 @@ class PersonToList extends Component {
     }
 }
 
-class PersonList extends Component {
-
+class PersonList2 extends Component {
     render() {
         const people = this.props.people;
         return people.map((person) => {
@@ -79,7 +123,7 @@ class PersonList extends Component {
                 id,
                 ...data
             } = person;
-            return <PersonToList {...data} key={id}/>
+            return <PersonToList2 {...data} key={id} />
         })
     }
 }
@@ -87,7 +131,7 @@ class PersonList extends Component {
 class App extends Component {
 
     render() {
-        return <PersonList people={people}/>
+        return <PersonList2 people={people} />
     }
 }
 
